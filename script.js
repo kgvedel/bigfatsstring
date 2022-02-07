00703b24d19537f1f58125be9d4cd231e15d4f8c
KatrineVedel <katr499k@stud.kea.dk>
2022-02-07 18:21:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//Mathilde and Katrine have been doing this assignment together and dying together #romeo&julietstyle
+
+
+let stringing = document.getElementById('input').value;
+let result;
+let dropdown = document.getElementById('dropdown').value;
+
+startForm();
+
+function startForm(){
+    
+document.querySelector("button").addEventListener("click", clickButton);
+
+}
+
+function clickButton(){
+
+stringing = document.getElementById('input').value;
+dropdown = document.getElementById('dropdown').value;
+
+console.log(stringing);
+document.removeEventListener("click", clickButton);
+
+changeValue();
+}
+
+function changeValue(){
+    dropdown.firstNamefind = stringing.toLowerCase();
+    upperAndlower = stringing.toUpperCase();
+result = dropdown;
+console.log(result);
+}
+
+
+   
+
+
+
