a2054da09aa4f9cbe23a6fb0fcbf5669e05afd76
KatrineVedel <katr499k@stud.kea.dk>
2022-02-10 09:04:14 +0100

    gg

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Mathilde and Katrine have been doing this assignment together and dying together #romeo&julietstyle
 
 
-const stringing = document.querySelector(".name").value;
+const stringing = document.querySelector(".input").value;
 const result;
-const dd1 = document.querySelector(".dropdown");
-const output = document.querySelector(".res");
+const dd1 = document.querySelector("dropdown");
+const output = document.querySelector(".output");
 const btn = document.querySelector("button");
 
-let name;
+
 
 startForm();
 
@@ -19,11 +19,16 @@ function startForm(){
 }
 
 function clickButton(){
+  let name;
 let selectValue = dd1.value;
 switch(selectValue){
     case "0":
-        name = stringing.value.toLowerCase();
+        name  = stringing.value.toLowerCase();
         output.value = `${name[0].toUpperCase()}${name.substring(1)}`;
+        break;
+        case "1":
+			name = input.value.substring(0, input.value.indexOf(" "));
+			output.value = name;
 }
 
 
